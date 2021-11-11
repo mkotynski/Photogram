e2a1890de668106bae6d1e6ea8e0bb0547d0f6aa
mkotynski <mateo.kotynski@gmail.com>
2021-11-11 17:03:19 +0100

    Initial view Added: - PostItem - PostList - StoriesItem - StoriesList - TheTopBar - TheSideBar - Home

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+        app
+        color="white"
+        style="border-bottom:1px solid #e2e2e2 !important;"
+        elevation="0"
+        height="50"
+    >
+      <div class="d-flex ml-2">
+        <span style="color:#414141">
+          {{ title }}
+        </span>
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+  data: () => ({
+    title: 'PHOTOGRAM',
+  }),
+});
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+  background-color: #fafafa;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.outline-border {
+  border: 1px solid #dbdbdb !important;
+  border-radius: 3px !important;
+  background-color:white !important;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-container class="mt-4">
+    <v-row class="justify-center">
+      <v-col cols="6">
+        <TheTopBar/>
+        <PostList/>
+      </v-col>
+
+      <v-col cols="3">
+        <TheSideBar style="border:1px solid red !important;"/>
+      </v-col>
+
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from 'vue'
+import PostList from "@/components/PostList.vue";
+import TheSideBar from "@/components/TheSideBar.vue";
+import TheTopBar from "@/components/TheTopBar.vue";
 
 export default Vue.extend({
   name: 'Home',
+
   components: {
-    HelloWorld,
+    TheTopBar,
+    TheSideBar,
+    PostList
   },
-});
+})
 </script>
